a91e708e8941ac598bd82f4a7e11a172696b2c0a
Artur Tsoi <xzoyart@gmail.com>
2024-04-08 10:18:59 +0500

    Refactor Input.java methods

--- app/src/main/java/hexlet/code/utilities/Input.java
+++ app/src/main/java/hexlet/code/utilities/Input.java
@@ -6,24 +6,11 @@ public class Input {
     public static String inputString(String textBeforeEntering) {
         Scanner scanner = new Scanner(System.in);
         System.out.print(textBeforeEntering);
-        String input = scanner.nextLine();
 
-        return input;
+        return scanner.nextLine();
     }
 
     public static String inputString() {
-        return inputString("");
-    }
-
-    public static int inputNumeric(String textBeforeEntering) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(textBeforeEntering);
-        int input = scanner.nextInt();
-
-        return input;
-    }
-
-    public static String inputNumeric() {
         return inputString("");
     }
 }
